1a83ca71161e0d4753c1fda249f7f223a696ef23
Guillaume Fine <90453342+Cosmeak@users.noreply.github.com>
2024-10-15 17:56:39 +0200

    fix(njord): issue with repeat key speed

--- hosts/njord/configuration.nix
+++ hosts/njord/configuration.nix
@@ -78,13 +78,13 @@
       		# Disable saving new documents to icloud
        	    NSDocumentSaveNewDocumentsToCloud = false;
             # Time before the key is repeated
-            InitialKeyRepeat = 1;
+            InitialKeyRepeat = 7;
             # Time between repeat key
-            KeyRepeat = 1;
+            KeyRepeat = 6;
             # Window open animation
             NSAutomaticWindowAnimationsEnabled = false;
             # Icon size in finder
-            NSTableViewDefaultSizeMode = 1;
+            NSTableViewDefaultSizeMode = 2;
             # Drag from anywhere a window to move it
             NSWindowShouldDragOnGesture = true;
             # Deativate sound bip when volume is changed
@@ -168,7 +168,6 @@
         "amethyst"
         # "sketchybar"
         "stats"
-        "logi-options-plus"
 
         # Fonts
         "sf-symbols"
